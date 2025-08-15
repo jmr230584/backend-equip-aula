b4915d1ebf393a8a32d90f090bc0a1eb79f38c48
José Mateus <mateusjoseph@gmail.com>
2025-08-15 08:34:17 -0300

    versionando Usuario

---
+++ src/appConfig.ts
@@ -0,0 +1,10 @@
+const SERVER_ROUTES = {
+
+    NOVO_USUARIO: '/novo/usuario',
+    ATUALIZAR_USUARIO: '/atualiza/usuario',
+    REMOVER_USUARIO: '/remove/usuario',
+    UNICO_USUARIO: '/lista/usuarioUnico',
+    LISTAR_USUARIO: '/lista/usuario'   
+     
+}
+export { SERVER_ROUTES }
